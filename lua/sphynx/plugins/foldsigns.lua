972f23d7378d6f63ab4e5a64f26c23210757acfa
sphynx79 <miboscol@gmail.com>
2025-02-18 22:45:38 +0100

    Refactoring: Aggiunto documentazione al plugin foldsign.nvim

--- lua/sphynx/plugins/foldsigns.lua
+++ lua/sphynx/plugins/foldsigns.lua
@@ -1,3 +1,27 @@
+--[[
+===============================================================================================
+Plugin: foldsigns.nvim
+===============================================================================================
+Description: Gestisce la visualizzazione dei segni (signs) nelle sezioni di codice piegate,
+             evitando che si sovrappongano o creino disordine visivo.
+
+Status: Active
+Author: lewis6991
+Repository: https://github.com/lewis6991/foldsigns.nvim
+
+Notes:
+ - Caricamento lazy tramite evento "file open"
+ - Integrazione automatica con il sistema di folding di Neovim
+ - Utile per mantenere pulita l'interfaccia quando si usano molti segni
+
+Keymaps:
+ - Nessuna mappatura specifica, funziona automaticamente
+
+TODO:
+    - Nessuna todo specifica, funziona automaticamente gia bene
+===============================================================================================
+--]]
+
 local M = {}
 
 M.plugins = {
