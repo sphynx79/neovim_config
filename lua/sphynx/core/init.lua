34728c3f871702e2fdd9680cd7363b13c1491699
sphynx79 <miboscol@gmail.com>
2025-02-25 05:50:27 +0100

    SAS

--- lua/sphynx/core/init.lua
+++ lua/sphynx/core/init.lua
@@ -13,3 +13,40 @@ vim.api.nvim_create_autocmd("User", {
         require("sphynx.utils.view").setup()
     end,
 })
+
+
+-------  TESTING ---------
+-- Configurazione base per il wrapping
+vim.opt.wrap = true                -- Abilita il wrapping
+vim.opt.linebreak = true           -- Wrap alle parole invece che ai caratteri
+vim.opt.breakindent = true         -- Mantiene l'indentazione nel wrap
+vim.opt.display = "lastline"       -- Mostra il più possibile dell'ultima linea
+vim.opt.textwidth = 0              -- Disabilita la formattazione automatica
+vim.opt.wrapmargin = 0             -- Disabilita il margine di wrapping
+
+-- Forza l'applicazione delle impostazioni per tutti i buffer
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+--   pattern = "*",
+--   callback = function()
+--     vim.opt_local.wrap = true
+--     vim.opt_local.linebreak = true
+--     vim.opt_local.breakindent = true
+--   end
+-- })
+
+-- -- Assicurati che il wrapping funzioni anche dopo il ridimensionamento
+-- vim.api.nvim_create_autocmd("VimResized", {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("redraw!")
+--   end
+-- })
+
+-- Crea comandi utili per gestire il wrapping
+vim.api.nvim_create_user_command('ToggleWrap', function()
+    vim.opt_local.wrap = not vim.opt_local.wrap:get()
+    print("Wrap è " .. (vim.opt_local.wrap:get() and "attivo" or "disattivo"))
+end, {})
+
+-- Mappatura rapida per toggleare il wrap
+vim.keymap.set('n', '<leader>w', ':ToggleWrap<CR>', { noremap = true, silent = true })
