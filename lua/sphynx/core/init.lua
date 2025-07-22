5165581aa953fcce79374136a18900739009b924
ichelema <miboscol@gmail.com>
2025-07-22 07:19:58 +0200

    Changed: cambiato vim.tbl_flatten per deprecata

--- lua/sphynx/core/init.lua
+++ lua/sphynx/core/init.lua
@@ -3,7 +3,6 @@ require("sphynx.core.1-settings")
 require("sphynx.core.2-autocommands")
 require("sphynx.core.3-plugins")
 
-
 vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()
