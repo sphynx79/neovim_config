2e7b08f5ca866bcc99f140fa203808a1bf8a5c91
ichelema <miboscol@gmail.com>
2025-07-22 07:16:26 +0200

    Added: Aggiunto supporto per luarock a Lazy

--- lua/sphynx/core/3-plugins.lua
+++ lua/sphynx/core/3-plugins.lua
@@ -222,10 +222,26 @@ Plugin_setup = (function ()
             -- increase downloads a lot.
             filter = true,
         },
-		  rocks = {
-			enabled = false,
-		},
-
+        pkg = {
+            enabled = true,
+            cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+            -- the first package source that is found for a plugin will be used.
+            sources = {
+            "lazy",
+            "rockspec", -- will only be used when rocks.enabled is true
+            "packspec",
+            },
+        },
+        rocks = {
+            enabled = true,
+            root = vim.fn.stdpath("data") .. "/lazy-rocks",
+            server = "https://nvim-neorocks.github.io/rocks-binaries/",
+            -- use hererocks to install luarocks?
+            -- set to `nil` to use hererocks when luarocks is not found
+            -- set to `true` to always use hererocks
+            -- set to `false` to always use luarocks
+            hererocks = nil,
+        },
         performance = {
 		    cache = {
 				enabled = true,
