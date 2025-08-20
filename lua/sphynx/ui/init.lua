3ce93b25cfd94b2543a2cd0529d6a1c78489daa1
ichelema <miboscol@gmail.com>
2025-08-20 06:23:05 +0200

    Ho incominciato a settare Tabby per la tabline

--- lua/sphynx/ui/init.lua
+++ lua/sphynx/ui/init.lua
@@ -1,6 +1,6 @@
 require("sphynx.colors").init()
 local config = sphynx.config
 
-if config.ui.tabufline.enabled then
-    require "sphynx.ui.tabufline.lazyload"
-end
+-- if config.ui.tabufline.enabled then
+--     require "sphynx.ui.tabufline.lazyload"
+-- end
