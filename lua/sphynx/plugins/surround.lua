7b145fc69431374ef8d3c8659e1249d7d6ae4770
ichelema <miboscol@gmail.com>
2026-06-04 17:31:46 +0200

    refactor(surround): corregge il refuso nel nome del modulo sorround -> surround
    Rinomina il file plugin e allinea la chiave M.plugins/M.configs e la voce in
    3-plugins.lua. Il plugin reale è kylechui/nvim-surround; "sorround" era solo un
    identificativo interno scritto male. Nessun cambiamento di comportamento.

--- lua/sphynx/plugins/surround.lua
+++ lua/sphynx/plugins/surround.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.plugins = {
-    ["sorround"] = {
+    ["surround"] = {
         "kylechui/nvim-surround",
         lazy = true,
         event = "VeryLazy",
@@ -9,7 +9,7 @@ M.plugins = {
 }
 
 M.configs = {
-    ["sorround"] = function()
+    ["surround"] = function()
         require("nvim-surround").setup()
     end,
 }
